76f6769ba0784aefcfe11f71ad50452f79ce3cd0
Daniel Martinez <mikedmtz@gmail.com>
2018-06-26 16:39:41 -0400

    center layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Flex, Container, Box, Text } from 'rebass/emotion';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>Strange</h1>
-    <p>There is no content here.</p>
-    <p>Perhaps there is an error somewhere?</p>
-  </div>
+  <Flex mt={5} flexWrap="wrap" justifyContent="center">
+    <Container w={0.6} fontSize={4}>
+      <Box>
+        <Text py={4} lineHeight={1.2}>
+          <h1>Strange</h1>
+          <p>There is no content here.</p>
+          <p>Perhaps there is an error somewhere?</p>
+        </Text>
+      </Box>
+    </Container>
+  </Flex>
 );
 
 export default NotFoundPage;
