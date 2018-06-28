207cb97eb85adb0558e73db7f6d5eb161080e333
Daniel Martinez <mikedmtz@gmail.com>
2018-06-28 19:08:28 -0400

    update blogPost template path

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,7 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
   return new Promise((resolve, reject) => {
-    const blogPostTemplate = path.resolve(`src/layouts/blog.js`);
+    const blogPostTemplate = path.resolve(`src/layouts/blogPost.js`);
     resolve(
       graphql(
         `
