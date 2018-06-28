2ec0d14c62c357e00dbfb16f62111ee06c039887
Daniel Martinez <mikedmtz@gmail.com>
2018-06-28 19:09:06 -0400

    refactor header

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -2,51 +2,37 @@ import React from 'react';
 import { navigateTo } from 'gatsby-link';
 import { Row, Column, Toolbar, NavLink, Text } from 'rebass/emotion';
 import styled from 'react-emotion';
-import { fontFamily } from 'styled-system';
 
-const Link = styled(NavLink)(
-  {
-    lineHeight: 1.2,
-    fontWeight: 'normal',
-  },
-  fontFamily
-);
+const Link = styled(NavLink)({
+  lineHeight: 1.2,
+  fontWeight: 'normal',
+});
 
-const LinkText = styled(Text)(
-  {
-    paddingTop: 10,
-    paddingBottom: 10,
-    transition: 'border-color 0.1s',
-    borderWidth: '0px',
-    borderBottom: '2px solid',
-    borderColor: 'transparent',
-    textTransform: 'capitalize',
-    fontWeight: 'normal',
-    '&:hover': {
-      borderColor: 'lightYellow',
-    },
+const LinkText = styled(Text)({
+  paddingTop: 10,
+  paddingBottom: 10,
+  transition: 'border-color 0.1s',
+  borderWidth: '0px',
+  borderBottom: '2px solid',
+  borderColor: 'transparent',
+  textTransform: 'capitalize',
+  fontWeight: 'normal',
+  '&:hover': {
+    borderColor: 'lightYellow',
   },
-  fontFamily
-);
+});
 
 const MyHeader = () => (
-  <Row bg="lightBlue" py={'3em'}>
+  <Row bg="lightBlue" py={'3em'} mb={2}>
     <Column mb={0}>
       <Toolbar bg="transparent">
-        <Link
-          color="white"
-          fontFamily="default"
-          fontSize={[5, '40px', 6]}
-          pl={4}
-          onClick={() => navigateTo('/')}
-        >
+        <Link color="white" fontSize={[5, '40px', 6]} pl={4} onClick={() => navigateTo('/')}>
           Daniel Martinez
         </Link>
         <NavLink
           onClick={() => navigateTo('/about')}
           ml="auto"
           mr={2}
-          fontFamily="default"
           fontWeight="normal"
           fontSize={4}
         >
