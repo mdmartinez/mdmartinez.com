f9c9ad1efe396c5aaeb860493d72cec547e3ccd3
Daniel Martinez <mikedmtz@gmail.com>
2018-07-02 10:45:33 -0400

    remove post variable

--- src/components/Post.js
+++ src/components/Post.js
@@ -16,19 +16,18 @@ import {
 import { withTheme } from 'emotion-theming';
 
 const Post = ({ data, theme }) => {
-  const post = data.markdownRemark;
   return (
     <Row>
       <Column>
         <Container w={[3 / 4, theme.widths.default]}>
           <Heading pb={[4]} fontSize={[4, 5]}>
-            {post.frontmatter.title}
+            {data.markdownRemark.frontmatter.title}
           </Heading>
           <Measure
             maxWidth={['48em']}
             fontSize={[3, 4]}
-            css={{ color: theme.colors.gray[8] }}
-            dangerouslySetInnerHTML={{ __html: post.html }}
+            css={{ color: theme.colors.grayScale[8] }}
+            dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
           />
         </Container>
       </Column>
