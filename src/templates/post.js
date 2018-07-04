306c10bdc73ad1d4edfa404909063c7ec45728b2
Daniel Martinez <mikedmtz@gmail.com>
2018-07-04 18:30:31 -0400

    feat(post.js): update header to use a chroma-js scale

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { Row, Column, Container, Heading, Measure, Text } from 'rebass/emotion';
-import { withTheme } from 'emotion-theming';
+import { Row, Column, Container, Heading, Measure } from 'rebass/emotion';
+import styled, { css } from 'emotion';
+import theme from '../theme';
+import chroma from 'chroma-js';
 
-const Post = ({ data, theme }) => {
+const orange = theme.colors.orangeSaturationScale[0];
+const gradients = chroma
+  .scale([orange, '#ffffff'])
+  .mode('lch')
+  .colors(7);
+
+const fade = css({
+  background: `linear-gradient(to right, ${gradients[0]}, ${gradients[1]} 15%, 
+    ${gradients[2]} 30%, ${gradients[3]} 45%, ${gradients[4]} 60%, 
+    ${gradients[5]} 75%, ${gradients[6]} 90%, transparent 100%)`,
+});
+
+const Post = ({ data }) => {
   const {
-    page: { html, timeToRead, frontmatter },
+    page: { html, frontmatter },
   } = data;
   return (
     <Row>
       <Column>
         <Container w={[3 / 4, theme.widths.default]}>
-          <Heading pb={[4]} fontSize={[4, 5]} css={{ fontFamily: theme.fonts.display }}>
+          <Heading
+            className={fade}
+            pl={1}
+            py={[3]}
+            mb={4}
+            fontSize={[4, 5]}
+            css={{ fontFamily: theme.fonts.display }}
+          >
+            {/* {JSON.stringify(scale)} */}
             {frontmatter.title}
           </Heading>
           <Measure
@@ -25,7 +47,7 @@ const Post = ({ data, theme }) => {
   );
 };
 
-export default withTheme(Post);
+export default Post;
 
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
