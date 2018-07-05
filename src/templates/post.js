50d540e9730abef4bca483a0bbf1e187643e4487
Daniel Martinez <mikedmtz@gmail.com>
2018-07-05 16:38:01 -0400

    refactor(post.js): update styling

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,49 +1,69 @@
 import React from 'react';
-import { Row, Column, Container, Heading, Measure } from 'rebass/emotion';
-import styled, { css } from 'emotion';
-import theme from '../theme';
+import { Row, Column, Container, Heading, Measure, Border } from 'rebass/emotion';
+import { css } from 'react-emotion';
 import chroma from 'chroma-js';
+import theme from '../theme';
 
-const orange = theme.colors.orangeSaturationScale[0];
-const gradients = chroma
-  .scale([orange, '#ffffff'])
-  .mode('lch')
-  .colors(7);
+const blue = theme.colors.blueGrayScale[1];
 
-const fade = css({
-  background: `linear-gradient(to right, ${gradients[0]}, ${gradients[1]} 15%, 
-    ${gradients[2]} 30%, ${gradients[3]} 45%, ${gradients[4]} 60%, 
-    ${gradients[5]} 75%, ${gradients[6]} 90%, transparent 100%)`,
+const gradients = [
+  `${blue},`,
+  `${chroma(blue)
+    .alpha(0.75)
+    .css('hsl')} 25%,`,
+  `${chroma(blue)
+    .alpha(0.35)
+    .css('hsl')} 50%,`,
+  `${chroma(blue)
+    .alpha(0.12)
+    .css('hsl')} 75%,`,
+  `${chroma(blue)
+    .alpha(0.01)
+    .css('hsl')} 100%`,
+];
+
+const bgGradient = css({
+  background: `linear-gradient(to right, ${gradients.join('')})`,
 });
 
 const Post = ({ data }) => {
   const {
     page: { html, frontmatter },
   } = data;
+
   return (
-    <Row>
-      <Column>
-        <Container w={[3 / 4, theme.widths.default]}>
-          <Heading
-            className={fade}
-            pl={1}
-            py={[3]}
-            mb={4}
-            fontSize={[4, 5]}
-            css={{ fontFamily: theme.fonts.display }}
-          >
-            {/* {JSON.stringify(scale)} */}
-            {frontmatter.title}
-          </Heading>
-          <Measure
-            maxWidth={['48em']}
-            fontSize={[3, 4]}
-            css={{ color: theme.colors.grayScale[8] }}
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        </Container>
-      </Column>
-    </Row>
+    <div>
+      <Row mt={-4} mb={4} py={5} className={bgGradient}>
+        <Column />
+        <Column my={0} w={[3 / 4, theme.widths.default]}>
+          <Container w={3 / 4}>
+            <Heading
+              color={theme.colors.blueGrayScale[7]}
+              mb={0}
+              ml={-4}
+              fontSize={[4, 6]}
+              css={{ fontFamily: theme.fonts.display }}>
+              {frontmatter.title}
+            </Heading>
+          </Container>
+        </Column>
+        <Column />
+      </Row>
+      <Row>
+        <Column />
+        <Column w={[3 / 4, theme.widths.default]}>
+          <Container w={3 / 4}>
+            <Measure
+              maxWidth={['48em']}
+              fontSize={[3, '20px']}
+              css={{ color: theme.colors.grayScale[8] }}
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </Container>
+        </Column>
+        <Column />
+      </Row>
+    </div>
   );
 };
 
