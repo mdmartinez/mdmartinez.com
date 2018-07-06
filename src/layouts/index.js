322260338314baf18f1f8552cf7c5f17d324faf7
Daniel Martinez <mikedmtz@gmail.com>
2018-07-06 15:49:15 -0400

    make layout stateful for clarity

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,43 +7,50 @@ import 'typeface-fira-sans';
 import '../css/global.css';
 import theme from '../theme';
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        {
-          name: 'description',
-          content: 'Making light of the obvious and the ordinary',
-        },
-        {
-          name: 'keywords',
-          content: 'experience, human, obvious, ordinary, meaningful, fulfillment',
-        },
-      ]}>
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="hsla(210, 100%, 25%, 1)" />
-      <meta name="apple-mobile-web-app-title" content="Daniel Martinez" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-57x57.png" sizes="57x57" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-72x72.png" sizes="72x72" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-76x76.png" sizes="76x76" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-114x114.png" sizes="114x114" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-120x120.png" sizes="120x120" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-144x144.png" sizes="144x144" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" sizes="152x152" />
-      <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" sizes="180x180" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
-      <noscript>JavaScript must be enabled to view this site.</noscript>
-    </Helmet>
-    <Provider theme={theme}>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      {children()}
-    </Provider>
-  </div>
-);
+class Layout extends React.Component {
+  render() {
+    const { children, location, data } = this.props;
+    let layoutHasHeader = location.pathname.match(/^\/posts\//) ? false : true;
+    return (
+      <div>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Making light of the obvious and the ordinary' },
+            {
+              name: 'keywords',
+              content: 'experience, human, obvious, ordinary, meaningful, fulfillment',
+            },
+          ]}>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="Daniel Martinez" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-57x57.png" sizes="57x57" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-72x72.png" sizes="72x72" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-76x76.png" sizes="76x76" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-114x114.png" sizes="114x114" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-120x120.png" sizes="120x120" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-144x144.png" sizes="144x144" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" sizes="152x152" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" sizes="180x180" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+          <noscript>JavaScript must be enabled to view this site.</noscript>
+        </Helmet>
+        <Provider theme={theme}>
+          <Header
+            siteTitle={data.site.siteMetadata.title}
+            headerVisible={layoutHasHeader}
+            location={location}
+          />
+          {children({ ...this.props })}
+        </Provider>
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
