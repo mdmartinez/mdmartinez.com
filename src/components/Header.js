c69bd834d1a5b1b585a36a0d9073677501e9a979
Daniel Martinez <mikedmtz@gmail.com>
2018-07-06 15:49:04 -0400

    Make about text stay active when clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { navigateTo } from 'gatsby-link';
 import { Row, Column, Toolbar, NavLink, Text } from 'rebass/emotion';
 import styled from 'react-emotion';
 import theme from '../theme';
 
-const LinkText = styled(Text)({
+const LinkText = styled(Text)(props => ({
   fontFamily: theme.fonts.display,
   color: theme.colors.blueGrayScale[0],
   transition: 'border-color 0.1s',
   borderWidth: '0px',
-  borderBottom: '2px solid',
-  borderColor: 'transparent',
   textTransform: 'capitalize',
+  borderBottom: props.isActive ? `2px solid ${theme.colors.oranges[1]}` : '2px solid transparent',
   '&:hover': {
     borderColor: theme.colors.oranges[1],
   },
-});
+}));
 
-const Header = () => (
-  <Row py="3em" mb={4} bg="blues.4">
-    <Column mb={0}>
-      <Toolbar bg="transparent">
-        <NavLink
-          onClick={() => navigateTo('/')}
-          pl={4}
-          color={theme.colors.blueGrayScale[0]}
-          css={{ fontFamily: theme.fonts.display }}
-          fontSize={[5, '40px', 6]}>
-          Daniel Martinez
-        </NavLink>
-        <NavLink onClick={() => navigateTo('/about')} ml="auto" mr={2} fontSize={[3, 4]}>
-          <LinkText pt={['12px', '20px']} pb={['5px']}>
-            about
-          </LinkText>
-        </NavLink>
-      </Toolbar>
-    </Column>
-  </Row>
-);
+const Header = ({ headerVisible = true, location }) => {
+  const isAbout = location.pathname.match(/^\/about/);
+  return (
+    <Row py="3em" mb={4} bg="blues.4" css={{ display: headerVisible ? 'flex' : 'none' }}>
+      <Column mb={0}>
+        <Toolbar bg="transparent">
+          <NavLink
+            onClick={() => navigateTo('/')}
+            pl={4}
+            color={theme.colors.blueGrayScale[0]}
+            css={{ fontFamily: theme.fonts.display }}
+            fontSize={[5, '40px', 6]}>
+            Daniel Martinez
+          </NavLink>
+          <NavLink onClick={() => navigateTo('/about')} ml="auto" mr={2} fontSize={[3, 4]}>
+            <LinkText isActive={isAbout} pt={['12px', '20px']} pb={['5px']}>
+              about
+            </LinkText>
+          </NavLink>
+        </Toolbar>
+      </Column>
+    </Row>
+  );
+};
 
 export default Header;
