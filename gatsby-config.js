8aaa90cb3b77e491110790f899dba959145bc862
Daniel Martinez <mikedmtz@gmail.com>
2018-07-13 07:53:28 -0400

    update manifest shortname

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Daniel Martinez',
-        short_name: 'Daniel Martinez',
+        short_name: 'Daniel M',
         start_url: '/',
         background_color: '#35AFE8',
         theme_color: '#E1673D',
