6961f7791340fdf17a0f15f9b3f1d36e896d3145
Daniel Martinez <mikedmtz@gmail.com>
2018-07-13 18:44:25 -0400

    move some header tags to layout file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -21,8 +21,24 @@ class Layout extends React.Component {
               name: 'keywords',
               content: 'experience, human, obvious, ordinary, meaningful, fulfillment',
             },
-          ]}
-        />
+          ]}>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="Daniel Martinez" />
+          <meata name="theme-color" content="#35AFE8" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-57x57.png" sizes="57x57" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-72x72.png" sizes="72x72" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-76x76.png" sizes="76x76" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-114x114.png" sizes="114x114" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-120x120.png" sizes="120x120" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-144x144.png" sizes="144x144" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-152x152.png" sizes="152x152" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-180x180.png" sizes="180x180" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+          <noscript>JavaScript must be enabled to view this site.</noscript>
+        </Helmet>
         <Provider theme={theme}>
           <Header siteTitle={data.site.siteMetadata.title} location={location} />
           {children({ ...this.props })}
