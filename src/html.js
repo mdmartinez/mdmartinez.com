f5de8d1354f7d879126e345d5ce8265071caa523
Daniel Martinez <mikedmtz@gmail.com>
2018-07-13 08:13:29 -0400

    add manifest.webmanifest to html.js

--- src/html.js
+++ src/html.js
@@ -21,6 +21,7 @@ module.exports = class HTML extends React.Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <link rel="manifest" href="/manifest.webmanifest" />
           <link rel="icon" href="/favicon.ico" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
