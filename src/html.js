2bb4a406f9948c66301b6a52208b7f8b9f667890
Daniel Martinez <mikedmtz@gmail.com>
2018-07-14 14:06:24 -0400

    remove links to fonts

--- src/html.js
+++ src/html.js
@@ -22,30 +22,6 @@ module.exports = class HTML extends React.Component {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="preload"
-            href="/static/2256e07a-0135-47d0-afbd-c63fcb1d4030.woff2"
-            as="font"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/static/8436c5b3-8af1-40f9-acd7-48dfdcaeac50.woff2"
-            as="font"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/static/52382db1-3de6-473e-b9c0-3cb5e70e80a2.woff2"
-            as="font"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/static/b03c5cd2-c6ad-4764-9cab-bf6211f181a7.woff2"
-            as="font"
-            crossOrigin="anonymous"
-          />
 
           {this.props.headComponents}
           {css}
