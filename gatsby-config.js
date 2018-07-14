9d881a934e5eb80cc26bad251e0048348f3a0491
Daniel Martinez <mikedmtz@gmail.com>
2018-07-13 20:36:54 -0400

    adjust ga plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
       options: {
         trackingId: 'UA-68492200-1',
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
