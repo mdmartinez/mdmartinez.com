8a33d481ce35a08b400c63f530771b8afc5f7011
Daniel Martinez <mikedmtz@gmail.com>
2018-07-19 20:59:38 -0400

    adjust tracking code

--- src/html.js
+++ src/html.js
@@ -24,7 +24,7 @@ export default class HTML extends React.Component {
           <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
           <script type="text/javascript" async="true" src="js/ga.js" />
-          {/* <script type="text/javascript" async="true" src="js/mp.js" /> */}
+          <script type="text/javascript" async="true" src="js/mp.js" />
           <noscript>JavaScript must be enabled to view this site.</noscript>
           {this.props.headComponents}
         </head>
@@ -33,9 +33,9 @@ export default class HTML extends React.Component {
 
           <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
-          {/* <script type="text/javascript" async="true" src="js/olark.js" /> */}
+          <script type="text/javascript" async="true" src="js/olark.js" />
           <script type="text/javascript" async="true" src="js/mtiFontTrackingCode.js" />
-          {/* <script type="text/javascript" async="true" src="js/heap.js" /> */}
+          <script type="text/javascript" async="true" src="js/heap.js" />
           <script type="text/javascript" src="js/register-sw.js" />
         </body>
       </html>
--- static/js/ga.js
+++ static/js/ga.js
@@ -1,44 +1,44 @@
-// function gaOptout() {
-//   (document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC;path=/'),
-//     (window[disableStr] = !0);
-// }
-// var gaProperty = 'UA-68492200-1',
-//   disableStr = 'ga-disable-' + gaProperty;
-// document.cookie.indexOf(disableStr + '=true') > -1 && (window[disableStr] = !0);
-// if (!(navigator.doNotTrack == '1' || window.doNotTrack == '1')) {
-//   (function(i, s, o, g, r, a, m) {
-//     i['GoogleAnalyticsObject'] = r;
-//     (i[r] =
-//       i[r] ||
-//       function() {
-//         (i[r].q = i[r].q || []).push(arguments);
-//       }),
-//       (i[r].l = 1 * new Date());
-//     (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-//     a.async = 1;
-//     a.src = g;
-//     m.parentNode.insertBefore(a, m);
-//   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-// }
-// if (typeof ga === 'function') {
-//   ga('create', 'UA-68492200-1', 'auto');
-//   ga('require', 'GTM-MR5DMHT');
-//   // ga('set', 'anonymizeIp', 1);
-// }
+function gaOptout() {
+  (document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC;path=/'),
+    (window[disableStr] = !0);
+}
+var gaProperty = 'UA-68492200-1',
+  disableStr = 'ga-disable-' + gaProperty;
+document.cookie.indexOf(disableStr + '=true') > -1 && (window[disableStr] = !0);
+if (!(navigator.doNotTrack == '1' || window.doNotTrack == '1')) {
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    (i[r] =
+      i[r] ||
+      function() {
+        (i[r].q = i[r].q || []).push(arguments);
+      }),
+      (i[r].l = 1 * new Date());
+    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+}
+if (typeof ga === 'function') {
+  ga('create', 'UA-68492200-1', 'auto');
+  ga('require', 'GTM-MR5DMHT');
+  // ga('set', 'anonymizeIp', 1);
+}
 
-(function(i, s, o, g, r, a, m) {
-  i['GoogleAnalyticsObject'] = r;
-  (i[r] =
-    i[r] ||
-    function() {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-    (i[r].l = 1 * new Date());
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-ga('create', 'UA-68492200-1', 'auto');
-ga('require', 'GTM-MR5DMHT');
-ga('send', 'pageview');
+// (function(i, s, o, g, r, a, m) {
+//   i['GoogleAnalyticsObject'] = r;
+//   (i[r] =
+//     i[r] ||
+//     function() {
+//       (i[r].q = i[r].q || []).push(arguments);
+//     }),
+//     (i[r].l = 1 * new Date());
+//   (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+//   a.async = 1;
+//   a.src = g;
+//   m.parentNode.insertBefore(a, m);
+// })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+// ga('create', 'UA-68492200-1', 'auto');
+// ga('require', 'GTM-MR5DMHT');
+// ga('send', 'pageview');
